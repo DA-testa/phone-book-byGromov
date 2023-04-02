586f4a7f3f39fa6d0fa51011ded2e1c94f8b77b5
Aleksandrs Pučenkins <90133296+byGromov@users.noreply.github.com>
2023-04-02 21:11:59 +0000

    updated_code1

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 # python3
 # Author: Aleksandrs Pučenkins 17.gr. 221RDB335
 
+class Contact:
+    def __init__(self, number, name):
+        self.number = number
+        self.name = name
+
 class Query:
     def __init__(self, query):
         self.type = query[0]
@@ -17,32 +22,20 @@ def write_responses(result):
 
 def process_queries(queries):
     result = []
-    # Keep list of all existing (i.e. not deleted yet) contacts.
-    contacts = []
+    # Keep dictionary of all existing (i.e. not deleted yet) contacts.
+    contacts = {}
     for cur_query in queries:
         if cur_query.type == 'add':
-            # if we already have contact with such number,
-            # we should rewrite contact's name
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    contact.name = cur_query.name
-                    break
-            else: # otherwise, just add it
-                contacts.append(cur_query)
+            contacts[cur_query.number] = Contact(cur_query.number, cur_query.name)
         elif cur_query.type == 'del':
-            for j in range(len(contacts)):
-                if contacts[j].number == cur_query.number:
-                    contacts.pop(j)
-                    break
+            contacts.pop(cur_query.number, None)
         else:
-            response = 'not found'
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    response = contact.name
-                    break
-            result.append(response)
+            response = contacts.get(cur_query.number, None)
+            if response is not None:
+                result.append(response.name)
+            else:
+                result.append('not found')
     return result
 
 if __name__ == '__main__':
     write_responses(process_queries(read_queries()))
-
